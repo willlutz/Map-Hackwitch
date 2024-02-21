c5e146508d59f68a7cf9a65f68caad9c4686846b
will lutz <wlutz@bsd220.org>
2024-02-21 09:35:16 -0600

    Initial Commit

---
+++ Map Hackwitch/Map_HackwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_HackwitchApp.swift
+//  Map Hackwitch
+//
+//  Created by will lutz on 2/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Map_HackwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
